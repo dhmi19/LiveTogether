ced5da11664b2dda980ec8fd9b1870d00dc2771a
Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
2020-05-20 21:12:57 -0400

    UI changes to grocery page

--- lib/screens/shoppingListScreen/GroceryItemTile.dart
+++ lib/screens/shoppingListScreen/GroceryItemTile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class GroceryItemTile extends StatelessWidget {
+class GroceryItemTile extends StatefulWidget {
 
   final String item;
   final int quantity;
@@ -9,10 +10,29 @@ class GroceryItemTile extends StatelessWidget {
   GroceryItemTile({this.item, this.quantity, this.description});
 
   @override
+  _GroceryItemTileState createState() => _GroceryItemTileState();
+}
+
+class _GroceryItemTileState extends State<GroceryItemTile> {
+
+  int newQuantity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    newQuantity = widget.quantity;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        print("Tapped");
+      onLongPress: (){
+        showDialog(
+            context: context,
+            builder: (BuildContext context){
+              return DeleteItemAlertBox(itemName: widget.item,);
+            }
+        );
       },
       child: Container(
         padding: const EdgeInsets.all(2),
@@ -28,19 +48,38 @@ class GroceryItemTile extends StatelessWidget {
                 )
             ),
 
-            Text(item, style: TextStyle(fontSize: 20),),
+            Text(widget.item, style: TextStyle(fontSize: 25),),
 
             SizedBox(height: 10,),
 
-            Text(quantity.toString(), style: TextStyle(fontSize: 20),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: (){
+                    setState(() {
+                      if(newQuantity > 0){
+                        newQuantity --;
+                      }
+                    });
+                  },
+                  icon: FaIcon(FontAwesomeIcons.minusCircle, size: 20, color: Theme.of(context).colorScheme.onSecondary,),
+                ),
+                Text(newQuantity.toString(), style: TextStyle(fontSize: 30),),
+                IconButton(
+                  onPressed: (){
+                    setState(() {
+                      newQuantity ++;
+                    });
+                  },
+                  icon: Icon(Icons.add_circle, size: 25, color: Theme.of(context).colorScheme.onSecondary,),
+                ),
+              ],
+            ),
 
             SizedBox(height: 10,),
 
-            Text("Description: ", style: TextStyle(fontSize: 15),),
-
-            SizedBox(height: 10,),
-
-            Text(description, style: TextStyle(fontSize: 15),),
+            Text(widget.description, style: TextStyle(fontSize: 15),),
 
           ],
         ),
@@ -50,6 +89,39 @@ class GroceryItemTile extends StatelessWidget {
         ),
 
       ),
+    );
+  }
+}
+
+class DeleteItemAlertBox extends StatelessWidget {
+
+  final String itemName;
+  DeleteItemAlertBox({this.itemName});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text("Remove $itemName?"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+
+              Navigator.of(context).pop();
+            },
+            textColor: Theme.of(context).primaryColor,
+            child: const Text('YES', style: TextStyle(fontSize: 18)),
+          ),
+
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            textColor: Theme.of(context).primaryColor,
+            child: Text('NO', style: TextStyle(fontSize: 18),
+            ),
+          ),
+
+        ],
     );
   }
 }
